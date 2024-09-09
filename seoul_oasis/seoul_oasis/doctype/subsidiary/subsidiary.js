ac74ba4e926a688e38de5560c672a0b5b460140a
jiyad11 <jiyadmuhammed11@gmail.com>
2024-09-09 17:56:08 +0530

    subsidiary:removed address and primary addrees, added new address field

--- seoul_oasis/seoul_oasis/doctype/subsidiary/subsidiary.js
+++ seoul_oasis/seoul_oasis/doctype/subsidiary/subsidiary.js
@@ -2,37 +2,37 @@
 // For license information, please see license.txt
 
 
-frappe.ui.form.on('Subsidiary', {
-	setup: function(frm) {
-		frm.set_query('address', function(doc) {
-			// if(!doc.subsidiary) {
-			// 	frappe.throw(__('Please set Company'));
-			// }
+// frappe.ui.form.on('Subsidiary', {
+// 	setup: function(frm) {
+// 		frm.set_query('address', function(doc) {
+// 			// if(!doc.subsidiary) {
+// 			// 	frappe.throw(__('Please set Company'));
+// 			// }
 
-			return {
-				query: 'frappe.contacts.doctype.address.address.address_query',
-				filters: {
-					link_doctype: 'Subsidiary',
-					link_name: frm.doc.name
-				}
-			};
-		});
-	},
-	address: function(frm){
-		if(frm.doc.address){
-			frappe.call({
-				method: 'frappe.contacts.doctype.address.address.get_address_display',
-				args: {
-					"address_dict": frm.doc.address
-				},
-				callback: function(r) {
-					console.log(r.message)
-					frm.set_value("primary_address", r.message);
-				}
-			});
-		}
+// 			return {
+// 				query: 'frappe.contacts.doctype.address.address.address_query',
+// 				filters: {
+// 					link_doctype: 'Subsidiary',
+// 					link_name: frm.doc.name
+// 				}
+// 			};
+// 		});
+// 	},
+// 	address: function(frm){
+// 		if(frm.doc.address){
+// 			frappe.call({
+// 				method: 'frappe.contacts.doctype.address.address.get_address_display',
+// 				args: {
+// 					"address_dict": frm.doc.address
+// 				},
+// 				callback: function(r) {
+// 					console.log(r.message)
+// 					frm.set_value("primary_address", r.message);
+// 				}
+// 			});
+// 		}
 		
-        },
-});
+//         },
+// });
 
 
